1b00b1e5b55e86123f7adc0a6f5862f78ae898e6
Trace <tracedelange@me.com>
2021-11-03 18:13:37 -0500

    Switched to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_03_161822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaign_users", force: :cascade do |t|
     t.integer "campaign_id"
     t.integer "user_id"
