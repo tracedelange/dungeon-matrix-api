ed2b97ef0078f9593b17c00aaa6f86e942264d2c
Trace <tracedelange@me.com>
2021-11-08 18:48:21 -0600

    Config changes for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.web_socket_server_url = "wss://dungeon-matrix-api.herokuapp.com/cable" 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
